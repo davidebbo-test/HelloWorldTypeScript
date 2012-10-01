45a3596a358e82eb41d608ab704896222a945de8
davidebbo <david.ebbo@microsoft.com>
2012-10-01 14:28:44 -0700

    Rename HttpServer.ts to server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import http = module("http");
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World using TypeScript\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
